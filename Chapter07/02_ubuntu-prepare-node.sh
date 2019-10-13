514fa89b18304af2685a24a5ab4a849f893289bf
Piotr Tylenda <tylenda.piotr@gmail.com>
2019-10-14 00:16:09 +0200

    Chapter 7: extending node preparation script

--- Chapter07/02_ubuntu-prepare-node.sh
+++ Chapter07/02_ubuntu-prepare-node.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get dist-upgrade -y
 apt-get install apt-transport-https ca-certificates curl software-properties-common ebtables ethtool -y 
 
+# Configure OS
 swapoff -a
 sed -e '/swap/ s/^#*/#/' -i /etc/fstab
 
@@ -20,3 +21,31 @@ EOF
 
 modprobe br_netfilter
 sysctl --system
+
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+apt-get update
+apt-get install docker-ce=5:18.09.9~3-0~ubuntu-bionic -y
+apt-mark hold docker-ce
+
+cat << EOF > /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+systemctl daemon-reload
+systemctl restart docker
+
+docker run hello-world
