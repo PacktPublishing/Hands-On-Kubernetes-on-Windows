6527a387554dbe78d879836e34f29cc8b2e18557
Piotr Tylenda <tylenda.piotr@gmail.com>
2019-10-14 23:26:33 +0200

    Chapter 7: Pod network installation

--- Chapter07/05_ubuntu-install-pod-network.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f kube-flannel.yaml
---
+++ Chapter07/06_ubuntu-install-pod-network.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kubectl apply -f https://raw.githubusercontent.com/PacktPublishing/Hands-On-Kubernetes-on-Windows/master/Chapter07/05_pod-network/kube-flannel-host-gw.yaml
